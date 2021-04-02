b5828d48ff81b4f410382b672478feb9840005de
alvinlal <alvinzzz2001@gmail.com>
2021-04-02 21:07:32 +0530

    comments and variables

---
+++ variables.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo Our shell name is$BASH                  # this is a system variable
+echo Our shell version name is $BASH_VERSION # this is a system variable
+echo Our home directory is $HOME             # this is a system variable
+echo Our current working directory is $PWD   # this is a system variable
+
+name="alvin" # this is a user defined variable
+age=2        # this is a user defined variable
+value=10     # this is a user defined variable
+
+echo My name is $name
+echo My age is $age
+echo value is $value
