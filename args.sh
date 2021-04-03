608cf47f8378a2ae7d0ecf30c3e16c5f3bf23453
alvinlal <alvinzzz2001@gmail.com>
2021-04-03 16:35:51 +0530

    arguments

---
+++ args.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "The first arguments is $1" # $1 holds the first argument, $2 the second and so on...
+echo "The second arguments is $2"
+echo "The third arguments is $3"
+echo "The zeroth argument is $0" # $0 holds the name of the file
+
+echo "The arguments as an array is $@" # $@ holds the arguments as an array
